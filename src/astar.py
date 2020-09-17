a80914b22db01c31ee7e4e3652fa8ca44945c4bd
Jafagervik <jorgenafagervik@gmail.com>
2020-09-17 22:11:22 +0200

    srtjøgn

--- src/astar.py
+++ src/astar.py
@@ -3,44 +3,36 @@ from node import Node
 from math import abs
 
 def manhattan_distance(start, end):
-    return abs(start.x - end.x) + abs(start.y - end.y)
+    return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
 def eucledian_distance(start, end):
-    pass
-
-def shortest(x, y):
-    pass
+    return sqrt((end[0]-start[0])**2+(end[1]-start[1])**2)
 
 
-def best_fist_search(start):
+def astar(start, end, temp=None):
     """
     Combines dfs and bfs into a more optimized way of searching
     :param start, Node, start position
     """
-
+    [0, 2]
 
 
     start.g = 0
     start.h = manhattan_distance(start, None) # Erstatt None med end node omsider
 
-    start.f = start.g + start.h
     open_nodes = [start]
     closed_nodes = []
 
     while open_nodes:
-        x = open_nodes.pop()
+        curr = open_nodes.pop()
         closed_nodes.append(x)
-        if x == end:
+        if curr == end:
             return
 
         for neighbour in start.children:
-            if neighbour not in dirs or neighbourn in closed:
+            if neighbour not in [(1,0), (0,1), (0, -1), (-1, 0)] or neighbour in closed:
                 break
 
             if None:
                 neighbour.f = manhattan_distance((0,0), (4,5))
-                neighbour.parent
-
-
-def astar():
-    return
+                neighbour.parent 
