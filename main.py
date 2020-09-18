15a538f693add350521e8dea455277924bc81922
tommy <tommyew7@gmail.com>
2020-09-18 15:36:52 +0200

    p

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         map_obj = Map_Obj(task)
         path = astar(map_obj.get_start_pos(), map_obj.get_end_goal_pos(), map_obj)
         for pos in path:
-            map_obj.set_cell_value(pos, '.')
+            map_obj.set_cell_value(pos, 'p')
         map_obj.show_map()
 
 if __name__ == '__main__':
