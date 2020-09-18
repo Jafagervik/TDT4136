44c39de3bf12a114f29ca8a2b135e6c096e7e98a
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2020-09-18 16:39:37 +0200

    Update astar.py

--- src/astar.py
+++ src/astar.py
@@ -2,14 +2,25 @@
 from math import inf, sqrt
 from src.node import Node
 
+# Two different ways of finding the heuristic
 def manhattan_distance(start, end):
+    """
+    >>> inp: [0,3], [5,5]
+    """
     return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
 def eucledian_distance(start, end):
+    """
+    >>> inp: [0,3], [5,5]
+    """
     return sqrt((end[0]-start[0])**2+(end[1]-start[1])**2)
 
 
 def backtrack(current_node):
+    """
+    When arriving at the end node, 
+    find the path by backtracking until arriving at the start node
+    """
     path = [current_node.pos]
     while current_node.parent:
         current_node = current_node.parent
@@ -19,12 +30,14 @@ def backtrack(current_node):
 def astar(start, end, map_obj):
     """
     Combines dfs and bfs into a more optimized way of searching
-    :param start, Node, start position
+    :param start, list[int], start position
+    :param end, list[int], end position
+    :param map_obj, Map_Obj, this is to have access to the cell values
     """
     start_node = Node(start, None)
     end_node = Node(end, None)
 
-    start_node.h = manhattan_distance(start, end) # Erstatt None med end node omsider
+    start_node.h = manhattan_distance(start, end) 
     start_node.f = start_node.g + start_node.h
 
     opened = [start_node]
@@ -37,14 +50,16 @@ def astar(start, end, map_obj):
 
         if curr_node.pos == end_node.pos:
             return backtrack(curr_node)
-
+           
+         # Generate neighbors for the current node
         (x,y) = curr_node.pos
         neighbors = [[x-1,y],[x,y-1],[x,y+1], [x+1,y]]
 
         for nxt in neighbors:
+            # the map value basically becomes the cost of travelling to that cell
             map_val = map_obj.get_cell_value(nxt)
 
-            # Do we hit a wall
+            # Check if we hit a wall
             if map_val == -1:
                 continue
 
@@ -60,6 +75,9 @@ def astar(start, end, map_obj):
                 opened.append(neighbor)
 
 def add_to_open(opened, neighbor):
+    """
+    Check if neighbor is cheap enough and in the opened list
+    """
     for node in opened:
         if neighbor == node and neighbor.f >= node.f:
             return False
